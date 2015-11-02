09771888f36976385ed306b6667f269f2dcb8375
Vladislav <v.kirbaba@gmail.com>
2015-11-02 11:07:05 +0300

    construction

---
+++ footer.php
@@ -0,0 +1,17 @@
+<footer class="footer">
+	<div class="container-fluid">
+		<div class="row">
+			
+		</div>
+	</div>
+</footer>
+    <!-- /#page-content-wrapper -->
+    <script>
+        jQuery ("#menu-toggle").click(function(e) {
+            e.preventDefault();
+            jQuery ("#wrapper").toggleClass("toggled");
+        });
+    </script>
+<?php wp_footer(); ?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,29 @@
+﻿
+<? get_header()?>
+<div id="wrapper">
+    <!-- Sidebar -->
+    <div id="sidebar-wrapper">	
+		<!-- Nav tabs -->
+		<ul class="nav nav-pills">
+			<li class="active"><a href="#home" data-toggle="pill">Главная</a></li>
+			<li><a href="#profile" data-toggle="pill">Профиль</a></li>
+			<li><a href="#messages" data-toggle="pill">Сообщения</a></li>
+
+		</ul>
+	</div>
+	<!-- /#sidebar-wrapper -->
+
+	<!-- Page Content -->
+	<div id="page-content-wrapper">
+		<div class="container-fluid">
+
+			<!-- Tab panes -->
+			<div class="tab-content">
+				<div class="tab-pane active" id="home"> Tab 1</div>
+				<div class="tab-pane" id="profile"> Tab 2</div>
+				<div class="tab-pane" id="messages"> Tab 3</div>	
+			</div>
+		</div>
+	</div>
+</div>
+<? get_footer()?>
